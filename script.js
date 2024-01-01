0b7cbfa530513c19de15b3dd633d3a88af684a82
Am1d1v <dimadiman1adima@gmail.com>
2024-01-01 11:17:35 +0300

    .

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const sun = document.querySelector('.sun');
 const scene = document.querySelector('.scene');
 
 
-//
+// Toggle between nightmode and daymode
 sun.addEventListener('click', () => {
     scene.classList.toggle('night');
 });
+
+setTimeout(() => {
+    alert('Click the sun to toggle the timemode');
+}, 0);
